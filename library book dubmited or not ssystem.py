df363bd3c17f354eb8d82bd67cfddbdd3cac0a11
adharsh.U <114822057+adharsh277@users.noreply.github.com>
2023-06-20 20:24:44 +0530

    Create library book dubmited or not ssystem.py

---
+++ library book dubmited or not ssystem.py
@@ -0,0 +1,11 @@
+c=int(input("enter your registration number: "))
+d=str(input("Enter the book name: "))
+e=int(input("Enter the reference number of the book: "))
+if(e==15676):
+  print("your book is submited ")
+  x="you have no due"
+  print(x.center(50))
+elif c==1232:
+  print("your book is submited  ")
+else:
+  print("you haven't submited the book \n if you didn't submit before the last day you have to pay fine for that")
