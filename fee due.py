cabc24d2714677bc08a7ed83d412f5f530e9416a
adharsh.U <114822057+adharsh277@users.noreply.github.com>
2023-06-21 20:58:14 +0530

    Create fee due.py

---
+++ fee due.py
@@ -0,0 +1,10 @@
+#fees due or NOt system
+a=int(input("Enter your registration number :"))
+b=str(input("Enter Your name :"))
+c=str(input("Enter your course :"))
+d=str(input("Enter your term : "))
+if(a==12219242):
+  print("your name is Ramesh \n you haven't pay the full amount your course fee ")
+  print("Chathhor vadekhil parijaatham")
+elif (a<10000):
+  print("you have paid your tutiob fee but remaining hostel and mess fee is still pending")
